d34b2f46e59cdb251fd6db0f00710ec74d4e8f7a
Iamneo <techadmin@iamneo.ai>
2023-08-06 14:36:13 +0000

    Backup Commit

--- reactapp/src/tests/Button.test.js
+++ reactapp/src/tests/Button.test.js
@@ -2,21 +2,19 @@ import React from "react";
 import Button from "../components/UI/Button/Button";
 import { shallow, mount } from "enzyme";
 
-describe('Testing Button Component', () => {
+describe("Testing Button Component", () => {
+  // render the button without any error
 
-    // render the button without any error
-    
-    test('testcase6', () => {
-        const wrapper = shallow(<Button />);
-        expect(wrapper.instance()).toBeDefined();
-    })
+  test("testcase8", () => {
+    const wrapper = shallow(<Button />);
+    expect(wrapper.instance()).toBeDefined();
+  });
 
-    // displays the correct text inside the component
+  // displays the correct text inside the component
 
-    test('testcase7', () => {
-        const TestingInput = 'Button Works'
-    const wrapper = shallow(<Button>{TestingInput}</Button>)
-        expect(wrapper.text()).toBe(TestingInput)
-    })
-
-})
+  test("testcase9", () => {
+    const TestingInput = "Button Works";
+    const wrapper = shallow(<Button>{TestingInput}</Button>);
+    expect(wrapper.props().children).toBe(TestingInput);
+  });
+});
--- reactapp/src/tests/Card.test.js
+++ reactapp/src/tests/Card.test.js
@@ -4,64 +4,59 @@ import { shallow, mount } from "enzyme";
 
 var questionProp, wrapper, mockFunction;
 
+describe("Testing Card Component", () => {
+  beforeEach(() => {
+    mockFunction = jest.fn();
 
+    questionProp = {
+      questionId: 2,
+      question: "What color is are the leaves ?",
+      option1: "Blue",
+      option2: "Red",
+      option3: "Yellow",
+      option4: "Green",
+      answer: "Green",
+    };
 
-describe('Testing Card Component', () => {
-    beforeEach(() => {
-        mockFunction = jest.fn();
+    wrapper = mount(
+      <Card
+        key={questionProp.questionId}
+        question={questionProp.question}
+        correctAnswerMarkUpdate={mockFunction}
+        attempt={mockFunction}
+        options={{
+          option1: questionProp.option1,
+          option2: questionProp.option2,
+          option3: questionProp.option3,
+          option4: questionProp.option4,
+        }}
+        answer={questionProp.answer}
+      />
+    );
+  });
 
-        questionProp = {
-            questionId : 2,
-            question : "What color is are the leaves ?",
-            option1 : "Blue",
-            option2 : "Red",
-            option3 : "Yellow",
-            option4 : "Green",
-            answer : "Green"
-        }
+  test("testcase10", () => {
+    expect(wrapper.instance()).toBeDefined();
+  });
 
-        wrapper = mount(<Card 
-            key={questionProp.questionId}
-            question={questionProp.question}
-            correctAnswerMarkUpdate={mockFunction}
-            attempt={mockFunction}
-            options={{
-            option1: questionProp.option1,
-            option2: questionProp.option2,
-            option3: questionProp.option3,
-            option4: questionProp.option4
-            }}
-            answer={questionProp.answer}
-        />);
-        
-    })
+  describe("Testing the display content of Card Component", () => {
+    // Check if the component displays the question properly
 
-
-    test('testcase8', () => {
-        expect(wrapper.instance()).toBeDefined();
+    test("testcase11", () => {
+      expect(wrapper.find("h4").text()).toBe("What color is are the leaves ?");
     });
 
-    describe('Testing the display content of Card Component', () => {
+    // check if all the options are rendered according to the test data
 
-        // Check if the component displays the question properly
+    test("testcase12", () => {
+      let optionsList = ["Blue", "Red", "Yellow", "Green"];
+      let optionIterator = 0;
+      expect(wrapper.find("button")).toHaveLength(4);
 
-        test('testcase9', () => {
-            expect(wrapper.find('h4').text()).toBe('What color is are the leaves ?');
-        })
-        
-        // check if all the options are rendered according to the test data
-
-        test('testcase10', () => {
-    
-            let optionsList = ['Blue', 'Red', 'Yellow', 'Green'];
-            let optionIterator = 0;
-            expect(wrapper.find('button')).toHaveLength(4);
-            
-            wrapper.find('button').forEach(node => {
-                expect(node.text()).toBe(optionsList[optionIterator]);
-                optionIterator++;
-            });
-        })
-    })
-
-})
+      wrapper.find("button").forEach((node) => {
+        expect(node.text()).toBe(optionsList[optionIterator]);
+        optionIterator++;
+      });
+    });
+  });
+});
