cb71e62f3b63c060c3e675a17315946023b181fa
Iamneo <techadmin@iamneo.ai>
2023-08-06 14:26:13 +0000

    Backup Commit

--- reactapp/src/tests/App.test.js
+++ reactapp/src/tests/App.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import App from "../App";
-import {  mount } from "enzyme";
+import { shallow, mount } from "enzyme";
 
 
 var wrapper;
@@ -28,7 +28,7 @@ describe('Testing App Component', () => {
     });
 
     test('testcase4', () => {
-        wrapper = mount(<App />);
+        const wrapper = mount(<App />);
         wrapper.find('button').simulate('click')
         wrapper.find('button').forEach(node => {
             expect(node.simulate('click'));
@@ -39,7 +39,7 @@ describe('Testing App Component', () => {
     });
 
     test('testcase5', () => {
-        wrapper = mount(<App />);
+        const wrapper = mount(<App />);
         wrapper.find('button').simulate('click')
         wrapper.find('button').forEach(node => {
             expect(node.simulate('click'));
