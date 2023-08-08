f790ee9a2b8d87ca0fdb067d1eac9e48f39a6bee
Iamneo <techadmin@iamneo.ai>
2023-08-08 08:52:12 +0000

    Backup Commit

--- reactapp/src/components/UI/Card/Card.jsx
+++ reactapp/src/components/UI/Card/Card.jsx
@@ -3,8 +3,10 @@ import Button from "../Button/Button";
 const Card = ({ question, correctAnswerMarkUpdate, attempt, options,answer,setQsns,qsn ,setCard,qsnAttempt}) => {
 // Creating an array of options
 const optionsArray = [options.option1, options.option2, options.option3,options.option4];
-// State variable to track the marked status of the buttonconst [marked, setMarked] = useState(false);
-// Event handler to handle the click event on the optionsconst click = (clicked) => {
+// State variable to track the marked status of the button
+const [marked, setMarked] = useState(false);
+// Event handler to handle the click event on the options
+const click = (clicked) => {
 // Increase the question attempted
 setQsns(qsn+1)
 // Mark the button as clicked
